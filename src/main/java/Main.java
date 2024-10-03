0c077085813cb302d3aa17eadf5a6cefe2a366ca
JBrincefield <jbrincefield@student.neumont.edu>
2024-10-02 18:11:45 -0600

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author jbrincefield
+ * @createdOn 10/2/2024 at 6:11 PM
+ * @projectName RDBL1
+ * @packageName PACKAGE_NAME;
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
