8dd7b68c313a5f7dd6f26cfeda69efb44889904a
JBrincefield <jbrincefield@student.neumont.edu>
2024-10-02 18:24:19 -0600

    made controllers
    title

---
+++ src/main/java/MongoController.java
@@ -0,0 +1,8 @@
+/**
+ * @author jbrincefield
+ * @createdOn 10/2/2024 at 6:19 PM
+ * @projectName RDBL1
+ * @packageName PACKAGE_NAME;
+ */
+public class MongoController {
+}
---
+++ src/main/java/neo4jController.java
@@ -0,0 +1,8 @@
+/**
+ * @author jbrincefield
+ * @createdOn 10/2/2024 at 6:22 PM
+ * @projectName RDBL1
+ * @packageName PACKAGE_NAME;
+ */
+public class neo4jController {
+}
